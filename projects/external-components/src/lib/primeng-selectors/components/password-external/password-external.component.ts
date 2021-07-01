6af5040c8fa80911a86a783319b25f213d437189
vinaydhakretutanota <vinaydhakre@tutanota.com>
2021-07-01 17:31:20 +0530

    Removed PrimeNg Model

--- projects/external-components/src/lib/primeng-selectors/components/password-external/password-external.component.ts
+++ projects/external-components/src/lib/primeng-selectors/components/password-external/password-external.component.ts
@@ -1,24 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PrimeNgPassword } from '../../models/primeng-password';
 
 @Component({
   selector: 'hmi-ext-password-external',
   templateUrl: './password-external.component.html',
   styleUrls: ['./password-external.component.scss']
 })
-export class PasswordExternalComponent implements OnInit {
+export class PasswordExternalComponent {
 
   @Input() fieldObj: any;
   @Input() dynamicAttributes: any;
 
-  fieldProperties: PrimeNgPassword;
-
-  constructor() {
-    this.fieldProperties = new PrimeNgPassword(null);
-  }
-
-  ngOnInit(): void {
-    this.fieldProperties = new PrimeNgPassword(this.fieldObj.customAttributes);
-  }
-
+  constructor() { }
 }
--- projects/external-components/src/lib/primeng-selectors/components/text-external/text-external.component.ts
+++ projects/external-components/src/lib/primeng-selectors/components/text-external/text-external.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PrimeNgText } from '../../models/primeng-text';
 
 
 @Component({
@@ -7,19 +6,10 @@ import { PrimeNgText } from '../../models/primeng-text';
   templateUrl: './text-external.component.html',
   styleUrls: ['./text-external.component.scss']
 })
-export class TextExternalComponent implements OnInit {
+export class TextExternalComponent {
 
   @Input() fieldObj: any;
   @Input() dynamicAttributes: any;
 
-  fieldProperties: PrimeNgText;
-
-  constructor() { 
-    this.fieldProperties = new PrimeNgText(null);
-  }
-
-  ngOnInit(): void {
-    this.fieldProperties = new PrimeNgText(this.fieldObj.customAttributes);
-  }
-
+  constructor() { }
 }
