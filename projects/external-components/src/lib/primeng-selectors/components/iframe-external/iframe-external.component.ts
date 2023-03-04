b06415579efae31ef1c49b4974df5b03382e3fbd
kpathe <pathe.kiran@gmail.com>
2023-03-04 19:40:29 +0530

    send projectId to iframe form-designer app

--- projects/external-components/src/lib/primeng-selectors/components/iframe-external/iframe-external.component.ts
+++ projects/external-components/src/lib/primeng-selectors/components/iframe-external/iframe-external.component.ts
@@ -1,5 +1,7 @@
-import {  Component, OnInit, AfterViewInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {  Component, OnInit, AfterViewInit, Inject, ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer } from "@angular/platform-browser";
+import { ActivatedRoute, Params } from '@angular/router';
 import { CommonExternalComponent } from '../common-external/common-external.component';
 
 
@@ -11,14 +13,31 @@ import { CommonExternalComponent } from '../common-external/common-external.comp
 export class IframeExternalComponent extends CommonExternalComponent  implements OnInit, AfterViewInit {
   src: any;
   visible= false;
-  constructor(private sanitizer: DomSanitizer ) { super()}
+  queryParams: Params = {};
+  @ViewChild('iframe') iframe: ElementRef | null = null;
+
+  constructor(private sanitizer: DomSanitizer, private readonly activatedRoute:ActivatedRoute) { 
+    super();
+  }
 
   ngOnInit(): void {
+    this.queryParams = this.activatedRoute.snapshot.queryParams;
+    // this.activatedRoute.queryParamMap
+    //   .subscribe((params) => {
+    //     console.log({ ...params.keys, ...params });
+    //   }
+    // );
+   
     this.src = this.sanitizer.bypassSecurityTrustResourceUrl(this.fieldObj.customAttributes.src);
 	//this.iframe.nativeElement.contentWindow.history.replaceState('WhatEverNameYouWant', "", this.src);
   }
-  ngAfterViewInit(): void {
-    this.visible= true;
+  ngAfterViewInit(): void {    
+    console.debug('sending query params to iframe');
+    this.iframe?.nativeElement.addEventListener('load', ()=> {
+      // remove * in production
+      this.iframe?.nativeElement.contentWindow?.postMessage(this.activatedRoute.snapshot.params,'*');
+    })
+    console.debug('query params sent to iframe');
   }
 
 }
