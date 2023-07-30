c4d3b4db2adbd0f30914e4308015b5312740d6a1
kpathe <pathe.kiran@gmail.com>
2023-07-30 16:49:49 +0530

    added dateTransformer for reports page

--- projects/external-components/src/lib/primeng-selectors/util/util.ts
+++ projects/external-components/src/lib/primeng-selectors/util/util.ts
@@ -1,4 +1,12 @@
-export const AppendText = (value:any)=> {
-    console.info('external ',value);
-    return value.map((v:any)=> ({...v, description: 'kiran'}));
+import { DatePipe } from "@angular/common";
+
+export const dateTransform = (value:any)=> {
+    const datePipe = new DatePipe('en-US');
+    const dateFormat = 'dd/MM/YYYY';
+    return value.map((v:any)=> {
+        const expectedStartDate = datePipe.transform(v.expectedStartDate, dateFormat);
+        const expectedEndDate = datePipe.transform(v.expectedEndDate, dateFormat);
+        return { ...v, expectedStartDate, expectedEndDate };
+    });
 }
+
