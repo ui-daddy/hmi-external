5d6076ad8341908aab8b019ab6975e28d03afa8d
RoniKundu <kkundu1995@gmail.com>
2023-08-04 21:34:37 +0530

    remove console.log

--- projects/external-components/src/lib/primeng-selectors/util/util.ts
+++ projects/external-components/src/lib/primeng-selectors/util/util.ts
@@ -13,7 +13,6 @@ export const dateTransform = (value:any)=> {
 export const multiDateTransform = (value:any)=> {
     const datePipe = new DatePipe('en-US');
     const dateFormat = 'dd/MM/YYYY';
-    console.log(JSON.stringify(value));
     return value.map((v:any)=> {
         const expectedStartDate = datePipe.transform(v.expectedStartDate, dateFormat);
         const expectedEndDate = datePipe.transform(v.expectedEndDate, dateFormat);
