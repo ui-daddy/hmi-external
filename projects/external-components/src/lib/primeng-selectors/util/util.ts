24371143f6c29a60c23f50f900f69303b5cce241
RoniKundu <kkundu1995@gmail.com>
2023-08-04 21:19:14 +0530

    remove payload

--- projects/external-components/src/lib/primeng-selectors/util/util.ts
+++ projects/external-components/src/lib/primeng-selectors/util/util.ts
@@ -13,7 +13,8 @@ export const dateTransform = (value:any)=> {
 export const multiDateTransform = (value:any)=> {
     const datePipe = new DatePipe('en-US');
     const dateFormat = 'dd/MM/YYYY';
-    return value.payload.map((v:any)=> {
+    console.log(JSON.stringify(value));
+    return value.map((v:any)=> {
         const expectedStartDate = datePipe.transform(v.expectedStartDate, dateFormat);
         const expectedEndDate = datePipe.transform(v.expectedEndDate, dateFormat);
         const actualStartDate = datePipe.transform(v.actualStartDate, dateFormat);
