18af7e94f33c9552fa7c59c4b9d5cf94d3ff3346
vinaydhakretutanota <vinaydhakre@tutanota.com>
2023-10-07 11:45:01 +0530

    Backport from pmTool to ui-daddy-develop

--- projects/external-components/src/lib/primeng-selectors/components/filter-group-external/filter-group-external.component.ts
+++ projects/external-components/src/lib/primeng-selectors/components/filter-group-external/filter-group-external.component.ts
@@ -7,6 +7,20 @@ export const FilterEventType = {
   DROPDOWN: 'dropdown'
 };
 
+export interface IFilterGroup {
+  label: string;
+  type: string;
+  value: string;
+  show: boolean;
+  labelKey?: string;
+  valueKey?: string;
+  filterApplied?: boolean;
+  optionList?: any[];
+  filterGroup?: IFilterGroup[];
+  pillLabel: string;
+  showLoader: boolean;
+}
+
 @Component({
   selector: 'hmi-ext-filter-group',
   templateUrl: './filter-group-external.component.html',
@@ -18,45 +32,32 @@ export class FilterGroupExternalComponent extends CommonExternalComponent implem
   private unlistener: any;
   showSubMenuOption: boolean;
   currentEventConfig: any;
+  filterLoader: boolean;
 
   constructor(private renderer2: Renderer2) { 
     super();
     this.showPopup = false;
     this.showSubMenuOption = false;
     this.items = [];
+    this.filterLoader = false;
   }
 
-  initializeCurrentEventConfig() {
-    this.currentEventConfig = {};
-    this.currentEventConfig[FilterEventType.DEPENDENT] = {
-      show: false,
-      value: [],
-      config: null
-    }
-
-    this.currentEventConfig[FilterEventType.TEXT] = {
-      show: false,
-      value: "",
-      config: null
-    }
-
-    this.currentEventConfig[FilterEventType.DROPDOWN] = {
-      show: false,
-      value: "",
-      config: null
-    }
+  filterFormGrp : IFilterGroup[] = [];
+  appliedFilterPills: IFilterGroup[] = [];
+  initializeFilterFormGrp() {
+    this.filterFormGrp = this.fieldObj.customAttributes?.filterOptions?.map((v: any)=> {
+      const data = { ...v, show: false };
+      if (v.type === FilterEventType.DEPENDENT) {
+        const filterGroup = v.filterGroup.map((dep:any)=> ({ ...dep, value: '' }))
+        return {...data, filterGroup}
+      }
+      return data;
+    });
+    console.info('filterFormGrp ',this.filterFormGrp)
   }
 
-  initializeDependentValue(length: number) {
-    for (let i = 0; i < length; i++) {
-      this.currentEventConfig[FilterEventType.DEPENDENT].value[i] = "";
-    }
-  }
-
-  hideFilterEventPopup() {
-    this.currentEventConfig[FilterEventType.DEPENDENT].show = false;
-    this.currentEventConfig[FilterEventType.TEXT].show = false;
-    this.currentEventConfig[FilterEventType.DROPDOWN].show = false;
+  hideFilterFormGrp() {
+    this.filterFormGrp = this.filterFormGrp.map(v=> ({...v, show: false}))
   }
 
   handleShowPopup() {
@@ -79,29 +80,54 @@ export class FilterGroupExternalComponent extends CommonExternalComponent implem
     this.unlistener();
   }
 
+  restoreValue() {
+    if (this.appliedFilterPills.length > 0) {
+      this.filterFormGrp = JSON.parse(JSON.stringify(this.appliedFilterPills));
+    }
+  }
+
   ngOnInit() {
-    console.log(this.fieldObj);
-    this.items = this.fieldObj.customAttributes?.filterOptions?.map((item: any) => {
+    this.initializeFilterFormGrp();
+
+    this.subscription = this.fieldObj.action.subscribe((actionObj: any) => {
+      if (actionObj.actionType === "CLEAR_COMPONENT_DATA") {
+        this.filterFormGrp.forEach((filter: IFilterGroup) => {
+          filter.filterApplied = false;
+        })
+        this.updateFilterPill();
+      } else if (actionObj.actionType === "RELOAD_COMPONENT_DATA") {
+        this.applyFilter();
+      }
+      if (actionObj.actionType === "SHOW_COMPONENT_LOADER") {
+        this.filterLoader = true;
+      }
+      if (actionObj.actionType === "HIDE_COMPONENT_LOADER") {
+        this.filterLoader = false;
+      }
+    });
+
+    this.items = this.fieldObj.customAttributes?.filterOptions?.map((item: any, index:number) => {
       item.command = () => {
-        this.initializeCurrentEventConfig();
+        this.restoreValue();
+        this.hideFilterFormGrp();
+        const filterObj = this.filterFormGrp[index];
         switch(item.type.toLowerCase()) {
           case FilterEventType.DEPENDENT:
-            this.currentEventConfig[FilterEventType.DEPENDENT].show = true;
-            this.currentEventConfig[FilterEventType.DEPENDENT].config = item;
-            this.initializeDependentValue(item.dependentList.length);
-            this.loadDropdownDataFromAPI((item.dependentList && item.dependentList[0]) || []);
+            filterObj.show = true;
+            if(filterObj.filterGroup && filterObj.filterGroup.length) {
+              this.loadDropdownDataFromAPI((filterObj.filterGroup[0]) || []);
+            }
             break;
           case FilterEventType.TEXT:
-            this.currentEventConfig[FilterEventType.TEXT].show = true;
-            this.currentEventConfig[FilterEventType.TEXT].config = item;
+            filterObj.show = true;
             break;
           case FilterEventType.DROPDOWN:
-            this.currentEventConfig[FilterEventType.DROPDOWN].show = true;
-            this.currentEventConfig[FilterEventType.DROPDOWN].config = item;
+            filterObj.show = true;
             this.loadDropdownDataFromAPI(item);
             break;
         }
         this.showSubMenuOption = true;
+        console.info('filterFormGrp ',this.filterFormGrp)
       }
       return item;
     }); 
@@ -113,68 +139,169 @@ export class FilterGroupExternalComponent extends CommonExternalComponent implem
     }
   }
 
-  loadData(ddOption: any) {     
-    this.customApiCall(ddOption.optionsConfig).subscribe((data: any) => {
-      ddOption.optionList = this.optionsFormatter(data, ddOption.optionsConfig);
+  loadData(ddOption: any, selectedValue?: string) {
+    ddOption.showLoader = true;
+    this.customApiCall(ddOption.optionsConfig).subscribe((data: any[]) => {
+      ddOption.optionList = data;
+      if (data && data.length && ddOption.filterOptionListBy) {
+        ddOption.optionList = ddOption.optionList.filter((v:any)=> v[ddOption.filterOptionListBy] === selectedValue);
+      }
+      ddOption.showLoader = false;
     }, ((err: any) => {
       ddOption.optionList = [];
       console.error(err);
+      ddOption.showLoader = false;
     }));
-  }
-
-  customGet(object: any, path: any, defaultValue: any) {
-    const pathArray = Array.isArray(path) ? path : path.split('.');
-    
-    for (const key of pathArray) {
-      if (object === null || typeof object !== 'object') {
-        return defaultValue;
-      }
-      
-      object = object[key];
-    }
-    
-    return object !== undefined ? object : defaultValue;
-  }
-
-  optionsFormatter(optionList: any[], optionConfig: any) {
-    if(optionList?.length) {
-
-      return optionList.map(optionObj => {
-        let modifiedOptionObj = {
-          label: this.customGet(optionObj, optionConfig.labelKey, ""),
-          value: this.customGet(optionObj, optionConfig.valueKey, "")
-        }
-        return modifiedOptionObj;
-      });
-    }
-    return [{label:"", value:""}];
   }
 
   closeSubMenuPopup() {
     this.showSubMenuOption = false;
   }
 
-  changeDependentDropdown(index: number) {
-    if (this.currentEventConfig[FilterEventType.DEPENDENT]?.config?.dependentList[index + 1]?.optionsConfig) {
-      this.loadData(this.currentEventConfig[FilterEventType.DEPENDENT]?.config?.dependentList[index + 1]);
+  changeDependentDropdown(index: number, selectedValue: any,list: any[]) {
+    if (list[index + 1]?.optionsConfig) {
+      this.clearOtherDependentDropdown(list, index + 2);
+      this.loadData(list[index + 1], selectedValue);
     }
   }
 
-  applyTextEvent() {
-    console.log(this.currentEventConfig[FilterEventType.TEXT]);
-    this.closePopup();
+  removeDependentDropdown(index: number, selectedValue: any,list: any[], event: any) {
+    event.stopPropagation();
+    if (list[index + 1]?.optionsConfig) {
+      list[index].value= null;
+      this.clearOtherDependentDropdown(list, index + 1);
+    }
   }
 
-  applyDropdownEvent() {
-    console.log(this.currentEventConfig[FilterEventType.TEXT]);
-    this.closePopup();
+  clearOtherDependentDropdown(list: any[], index: number) {
+    for (; index < list.length; index++) {
+      list[index].optionList = [];
+    }
   }
 
-  applyDependentEvent() {
-    console.log(this.currentEventConfig[FilterEventType.TEXT]);
+  updateFilterPill() {
+    this.appliedFilterPills = JSON.parse(JSON.stringify(this.filterFormGrp));
+  }
+
+  getFilterPillValue(filter: IFilterGroup): IFilterGroup {
+    filter.pillLabel = "";
+    if (filter.type === FilterEventType.DEPENDENT) {
+      filter.filterGroup?.forEach(((depF: IFilterGroup) => {
+        if (depF.value) {
+          filter.pillLabel += depF.label + ": " + depF.value + ", ";
+        }
+      }));
+      if (filter.pillLabel.length >=2 && filter.pillLabel[filter.pillLabel.length - 2] === ",") {
+        filter.pillLabel = filter.pillLabel.slice(0, filter.pillLabel.length - 2);
+      }
+    } else {
+      filter.pillLabel += filter.label + ": " + filter.value;
+    }
+    return filter;
+  }
+
+  applyTextEvent(filterObj: IFilterGroup) {
+    filterObj.filterApplied = true;
+    this.closePopup();
+    this.applyFilter();
+  }
+
+  applyDropdownEvent(filterObj: IFilterGroup) {
+    filterObj.filterApplied = true;
+    this.closePopup();
+    this.applyFilter();
+  }
+
+  applyDependentEvent(filterObj: IFilterGroup) {
+    filterObj.filterApplied = true;
+    this.closePopup();
+    this.applyFilter();
+  }
+
+  prepareData(): any[] {
+    let data: any = {};
+    this.filterFormGrp.forEach((filter: IFilterGroup) => {
+      if(filter.filterApplied) {
+        if (filter.type === 'dependent') {
+          filter.filterGroup?.forEach(((depF: IFilterGroup) => {
+            if (depF.valueKey) {
+              data[depF.valueKey] = depF.value;
+            }
+          }));
+        } else {
+          if (filter.valueKey) {
+            data[filter.valueKey] = filter.value;
+          }
+        }
+      }
+    });
+    return data;
+  }
+
+  applyFilter(isRemove?: boolean, filter?: IFilterGroup) {
+    this.filterLoader = true;
+    const data = this.prepareData(),
+          config = JSON.parse(JSON.stringify(this.fieldObj.customAttributes?.filterConfig));
+    config.queryParams = this.filterParams(config.queryParams, data);
+    config.pathParams = this.filterParams(config.pathParams, data);
+    config.payloadParams = this.filterParams(config.payloadParams, data);
+
+    if (this.fieldObj.customAttributes?.showComponentLoaderOnApply) {
+      this.initializeEvents.emit({ name: "fireEvent", events: [
+        {
+          "event": "click",
+          "actions": [{
+            "actionType": "SHOW_COMPONENT_LOADER",
+            "componentName": this.fieldObj.customAttributes?.showComponentLoaderOnApply
+          }]
+        }
+      ], data: null});
+    }
+
+    this.customApiCall(config, data).subscribe((data: any[]) => {
+      if (isRemove && filter) {
+        filter.filterApplied = false;
+        if (filter.type === FilterEventType.DEPENDENT) {
+          filter.filterGroup?.map((fObj) => { fObj.value = ''});
+        } else {
+          filter.value = '';
+        }
+      }
+      this.updateFilterPill();
+      this.hideComponentLoader();
+    }, ((err: any) => {
+      console.error(err);
+      this.hideComponentLoader();
+    }));
+  }
+
+  hideComponentLoader() {
+    this.filterLoader = false;
+    if (this.fieldObj.customAttributes?.showComponentLoaderOnApply) {
+      this.initializeEvents.emit({ name: "fireEvent", events: [
+        {
+          "event": "click",
+          "actions": [{
+            "actionType": "HIDE_COMPONENT_LOADER",
+            "componentName": this.fieldObj.customAttributes?.showComponentLoaderOnApply
+          }]
+        }
+      ], data: null});
+    }
+  }
+
+  filterParams(params: any, data: any): any[] {
+    return params?.length ? params.filter((param: any) => !!data[param.mappedValue]) : [];
+  }
+
+  removeFilter(filter: IFilterGroup, index: number) {
+    this.filterFormGrp[index].filterApplied = false;
+    this.applyFilter(true, this.filterFormGrp[index]);
   }
 
   ngOnDestroy(): void {
-    this.unlistener();
+    if (this.unlistener) {
+      this.unlistener();
+    }
   }
 }
