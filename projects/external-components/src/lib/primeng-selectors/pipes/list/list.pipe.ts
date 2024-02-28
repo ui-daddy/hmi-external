0e6d68792d09a7490ea7ec743b6f4fe3f3a2c4ce
RoniKundu <kkundu1995@gmail.com>
2024-02-28 21:26:30 +0530

    add filter

--- projects/external-components/src/lib/primeng-selectors/pipes/list/list.pipe.ts
+++ projects/external-components/src/lib/primeng-selectors/pipes/list/list.pipe.ts
@@ -4,12 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ListPipe implements PipeTransform {
   transform(arr: Array<any>, fieldName: string): Array<any> {
     arr = this.uniqArr(arr, fieldName);
-    return arr.map((v)=> {
-      if(v !== null && v !== "") {
-        console.log(v);
+    return arr.filter(value => value !== null && value !== undefined).map((v)=> {
         ({label: v , value: v });
-      }
-    } )
+    });
   }
 
   private uniqArr(arr: Array<any>,fieldName:string): Array<any> {
