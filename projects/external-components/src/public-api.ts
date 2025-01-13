a356c92aa4c8e677647a2964d0c428663657eaa8
Streetz Solutions <60189767+ui-daddy@users.noreply.github.com>
2025-01-13 09:33:19 +0530

    Commit multiple files

--- projects/external-components/src/public-api.ts
+++ projects/external-components/src/public-api.ts
@@ -20,3 +20,4 @@ export * from './lib/primeng-selectors/components/generate-with-ai/generate-with
  
 export * from './lib/primeng-selectors/components/barChart-external/barChart-external.component';
 export * from './lib/primeng-selectors/components/weather-external/weather-external.component';
+export * from './lib/primeng-selectors/components/metricAI-external/metricAI-external.component';
