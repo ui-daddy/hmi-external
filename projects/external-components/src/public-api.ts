08be337228fc8213ea7da168638451f5a4dfd558
ui-daddy <streetzsolutions@gmail.com>
2025-01-20 23:20:59 +0530

    Revert "Commit multiple files"
    This reverts commit 840a1315bd4982bfbb306ac6d011d9c9b85b09d1.

--- projects/external-components/src/public-api.ts
+++ projects/external-components/src/public-api.ts
@@ -20,4 +20,3 @@ export * from './lib/primeng-selectors/components/generate-with-ai/generate-with
  
 export * from './lib/primeng-selectors/components/barChart-external/barChart-external.component';
 export * from './lib/primeng-selectors/components/weather-external/weather-external.component';
-export * from './lib/primeng-selectors/components/aip2-external/aip2-external.component';
