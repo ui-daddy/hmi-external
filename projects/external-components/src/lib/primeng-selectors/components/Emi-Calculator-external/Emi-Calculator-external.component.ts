6f798377ff99b348c5987f3cf325ba466254669d
Streetz Solutions <60189767+ui-daddy@users.noreply.github.com>
2025-04-22 09:48:13 +0530

    programmatically updating json file on 4/22/2025, 4:18:13 AM by ui-daddy.

--- projects/external-components/src/lib/primeng-selectors/components/Emi-Calculator-external/Emi-Calculator-external.component.ts
+++ projects/external-components/src/lib/primeng-selectors/components/Emi-Calculator-external/Emi-Calculator-external.component.ts
@@ -17,7 +17,7 @@ import { CommonExternalComponent } from '../common-external/common-external.comp
         <option *ngFor="let year of years" [value]="year">{{ year }} Year(s)</option>
       </select>
 
-      <button (click)="calculateEMI()" style="width: 100%; padding: 10px; background-color: red; color: white; border: none; border-radius: 4px; cursor: pointer; transition: background-color 0.3s;">
+      <button (click)="calculateEMI()" style="width: 100%; padding: 10px; background-color: blue; color: white; border: none; border-radius: 4px; cursor: pointer; transition: background-color 0.3s;">
         Calculate EMI
       </button>
 
