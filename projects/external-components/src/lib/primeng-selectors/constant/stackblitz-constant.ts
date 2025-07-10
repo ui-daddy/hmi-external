101a9f188a595785fd53a03aa2467e2bf4b477f4
ui-daddy <streetzsolutions@gmail.com>
2025-07-10 22:29:23 +0530

    Added primeicons by default

--- projects/external-components/src/lib/primeng-selectors/constant/stackblitz-constant.ts
+++ projects/external-components/src/lib/primeng-selectors/constant/stackblitz-constant.ts
@@ -191,8 +191,8 @@ export const STACKBLITZ_DEPENDENCIES = {
     "tslib": "^2.0.0",
     "zone.js": "~0.14.10",
     "@popperjs/core": "^2.11.8",
-    "bootstrap": "^5.2.3"
-    
+    "bootstrap": "^5.2.3",
+    "primeicons": "^7.0.0"
 };
 
 export const STACKBLITZ_COMMON_EXTERNAL_TS = `
