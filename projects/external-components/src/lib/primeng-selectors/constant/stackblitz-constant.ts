d3a2a8c56ed0254245cb1845cea9f2eda4ba817f
krishnadaskundu <82699416+krishnadaskundu@users.noreply.github.com>
2025-07-26 19:08:05 +0530

    date with time component, customApiCall type change for stackblitz component, multiDateTimeTransform added in util.

--- projects/external-components/src/lib/primeng-selectors/constant/stackblitz-constant.ts
+++ projects/external-components/src/lib/primeng-selectors/constant/stackblitz-constant.ts
@@ -18,12 +18,13 @@ import { NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbTypeahea
 import { IconField } from 'primeng/iconfield';
 import { InputIcon } from 'primeng/inputicon';
 import { DialogModule } from 'primeng/dialog';
+import {DatePickerModule} from 'primeng/datepicker';
 
 export const IMPORT_MODULES = [
     CommonModule, RouterModule,FormsModule, ReactiveFormsModule, PasswordModule, InputTextModule,
     TableModule, DropdownModule, ToggleButtonModule, ChartModule, MultiSelectModule, ButtonModule, 
     TooltipModule, MenuModule, TagModule, DynamicDialog, ProgressSpinnerModule, IconField, InputIcon, 
-    NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbTypeaheadModule, DialogModule
+    NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbTypeaheadModule, DialogModule,DatePickerModule
 ];
 
 export const STACKBLITZ_IMPORT_MODULES = `
@@ -31,7 +32,7 @@ export const STACKBLITZ_IMPORT_MODULES = `
     BrowserModule, CommonModule, RouterModule,FormsModule, ReactiveFormsModule, PasswordModule, InputTextModule,
     TableModule, DropdownModule, ToggleButtonModule, MultiSelectModule, ButtonModule, 
     TooltipModule, MenuModule, TagModule, DynamicDialog, ProgressSpinnerModule, IconField, InputIcon,
-    NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbTypeaheadModule, DialogModule
+    NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbTypeaheadModule, DialogModule, DatePickerModule
 ]`;
 
 export const STACKBLITZ_IMPORT_STATEMENT = `
--- projects/external-components/src/lib/primeng-selectors/util/util.ts
+++ projects/external-components/src/lib/primeng-selectors/util/util.ts
@@ -14,11 +14,23 @@ export const multiDateTransform = (value:any)=> {
     const datePipe = new DatePipe('en-US');
     const dateFormat = 'dd/MM/YYYY';
     return value.map((v:any)=> {
-        const expectedStartDate = datePipe.transform(v.expectedStartDate, dateFormat);
+        const scheduledDate = datePipe.transform(v.scheduledDate, dateFormat);
         const expectedEndDate = datePipe.transform(v.expectedEndDate, dateFormat);
         const actualStartDate = datePipe.transform(v.actualStartDate, dateFormat);
         const actualEndDate = datePipe.transform(v.actualEndDate, dateFormat);
-        return { ...v, expectedStartDate, expectedEndDate, actualStartDate, actualEndDate };
+        return { ...v, scheduledDate, expectedEndDate, actualStartDate, actualEndDate };
+    });
+}
+
+export const multiDateTimeTransform = (value: any) => {
+    const datePipe = new DatePipe('en-US');
+    const dateTimeFormat = 'dd MMM yy, hh:mm a'; // e.g., 25 Jul 24, 03:45 PM
+    return value.map((v: any) => {
+        const scheduledDate = datePipe.transform(v.scheduledDate, dateTimeFormat);
+        const expectedEndDate = datePipe.transform(v.expectedEndDate, dateTimeFormat);
+        const actualStartDate = datePipe.transform(v.actualStartDate, dateTimeFormat);
+        const actualEndDate = datePipe.transform(v.actualEndDate, dateTimeFormat);
+        return { ...v, scheduledDate, expectedEndDate, actualStartDate, actualEndDate };
     });
 }
 
