bea99f52bd0ba93277fcaf0a8beffae896f22750
krishnadaskundu <82699416+krishnadaskundu@users.noreply.github.com>
2025-08-24 19:34:03 +0530

    remnoved unnecessary modles from STACKBLITZ_IMPORT_MODULES

--- projects/external-components/src/lib/primeng-selectors/constant/stackblitz-constant.ts
+++ projects/external-components/src/lib/primeng-selectors/constant/stackblitz-constant.ts
@@ -25,9 +25,7 @@ export const IMPORT_MODULES = [
 
 export const STACKBLITZ_IMPORT_MODULES = `
 [
-    BrowserModule, CommonModule, RouterModule,FormsModule, ReactiveFormsModule, PasswordModule, InputTextModule,
-    TableModule, DropdownModule, ToggleButtonModule, MultiSelectModule, ButtonModule, 
-    TooltipModule, MenuModule, TagModule, DynamicDialog, ProgressSpinnerModule
+    BrowserModule, CommonModule, RouterModule,FormsModule, ReactiveFormsModule
 ]`;
 
 export const STACKBLITZ_IMPORT_STATEMENT = `
