395e713cd5bb466e335170de471636add47ffb30
krishnadaskundu <82699416+krishnadaskundu@users.noreply.github.com>
2025-08-27 18:54:37 +0530

    Revert code

--- projects/external-components/src/lib/primeng-selectors/util/util.ts
+++ projects/external-components/src/lib/primeng-selectors/util/util.ts
@@ -14,11 +14,11 @@ export const multiDateTransform = (value:any)=> {
     const datePipe = new DatePipe('en-US');
     const dateFormat = 'dd/MM/YYYY';
     return value.map((v:any)=> {
-        const scheduledDate = datePipe.transform(v.scheduledDate, dateFormat);
+        const expectedStartDate = datePipe.transform(v.expectedStartDate, dateFormat);
         const expectedEndDate = datePipe.transform(v.expectedEndDate, dateFormat);
         const actualStartDate = datePipe.transform(v.actualStartDate, dateFormat);
         const actualEndDate = datePipe.transform(v.actualEndDate, dateFormat);
-        return { ...v, scheduledDate, expectedEndDate, actualStartDate, actualEndDate };
+        return { ...v, expectedStartDate, expectedEndDate, actualStartDate, actualEndDate };
     });
 }
 
